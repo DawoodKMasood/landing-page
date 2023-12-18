473a6c50d1d78a5f8a82b3346ff992409123ae94
dmasood-aslase <108358013+abdullahhcms@users.noreply.github.com>
2023-12-18 17:51:06 +0500

    Added `i18n` for internalization

---
+++ app/[locale]/page.js
@@ -0,0 +1,11 @@
+import Navbar from "@/components/shared/Navbar";
+
+const Home = ({ params: { locale } }) => {
+  return (
+    <main>
+      <Navbar language={locale} />
+    </main>
+  )
+}
+
+export default Home;
--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,9 +1,12 @@
-const Navbar = (props) => {
+import { useTranslation } from '../../i18n'
+
+const Navbar = async ({language}) => {
+    const { t } = await useTranslation(language)
 
     return (
-        <>
-
-        </>
+        <div>
+            
+        </div>
     )
 }
 
