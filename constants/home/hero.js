cfbc86b85dcd2558e5a0d2f3b4891a25b179ff43
abdullahhcms <91176669+DawoodKMasood@users.noreply.github.com>
2024-01-04 00:46:37 +0500

    Navbar & Hero Section

--- constants/home/hero.js
+++ constants/home/hero.js
@@ -5,24 +5,21 @@ const heroSlides = [
     heading: 'Web Wonders: ',
     title: 'Crafting Digital Realms',
     description:
-      'Transforming ideas into dynamic, user-friendly websites. Specializing in responsive design, e-commerce solutions, and custom CMS development.',
-    src: webDevelopmentAnimation,
+      'Transforming ideas into dynamic, user-friendly websites. Specializing in responsive design and CMS development.',
   },
   {
     id: 'app-development',
     heading: 'App Odyssey: ',
     title: 'Navigating Mobile Frontiers',
     description:
-      'Crafting cutting-edge mobile applications for iOS and Android. Focused on performance, user experience, and scalability.',
-    src: webDevelopmentAnimation,
+      'Navigating Mobile Frontiers: Crafting cutting-edge mobile applications for iOS and Android.',
   },
   {
     id: 'ux-ui-design',
     heading: 'Design Symphony: ',
     title: 'Harmonizing Form & Function',
     description:
-      'Creating intuitive and visually appealing user interfaces. Emphasizing user experience, interaction design, and prototyping.',
-    src: webDevelopmentAnimation,
+      'Creating intuitive and visually appealing user interfaces. Emphasizing user experience and prototyping.',
   },
 ];
 
